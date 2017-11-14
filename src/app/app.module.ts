1210ca29424aeaece6912a7c9cb59153f41fd075
Joshua Lewis <joshilewis@gmail.com>
2017-11-14 20:56:02 +0200

    adds routing and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { MaterialModuleModule } from './/material-module.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
