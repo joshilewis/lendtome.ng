87aec3a003fd8d7d538900374d99ebf9aca969bf
Joshua Lewis <joshilewis@gmail.com>
2017-11-14 21:20:50 +0200

    completes home component: adds nav buttons, with working templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'how-it-works', component: HowItWorksComponent },
+  { path: 'get-started', component: GetStartedComponent },
+  { path: 'sign-in', component: SignInComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { MaterialModuleModule } from './/material-module.module';
 import { HomeComponent } from './home/home.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HowItWorksComponent,
+    GetStartedComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
