64ed5139a7a72986d94f0abfb0781dc410efae5c
Joshua Lewis <joshilewis@gmail.com>
2017-11-22 17:13:58 +0200

    copy: Learn more page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
-import { GetStartedComponent } from './get-started/get-started.component';
 import { LibrariesComponent } from './libraries/libraries.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
@@ -12,7 +11,6 @@ const routes: Routes = [
   { path: 'user', component: UserProfileComponent },
   { path: 'home', component: HomeComponent },
   { path: 'how-it-works', component: HowItWorksComponent, canActivate: [AuthGuard] },
-  { path: 'get-started', component: GetStartedComponent, canActivate: [AuthGuard]  },
   { path: 'libraries', component: LibrariesComponent, canActivate: [AuthGuard]  },
 ];
 
