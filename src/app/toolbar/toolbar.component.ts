9e873376a2e88ef71ee0a0429af1ce7020dd4c0c
Joshua Lewis <joshilewis@gmail.com>
2017-11-23 20:55:02 +0200

    makes 'lend-to.me' in toolbar clickable

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,7 +8,7 @@ import { AuthService } from '../core/auth.service';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, public router: Router) {
     authService.user.subscribe(user => {
     });
   }
