699c99c8c41c70da49afd0ea8b412d9201727706
Joshua Lewis <joshilewis@gmail.com>
2017-11-24 15:32:49 +0200

    adds Add book by ISBN search to home page

--- src/app/material-module.module.ts
+++ src/app/material-module.module.ts
@@ -1,22 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 import {
   MatButtonModule,
   MatCheckboxModule,
   MatToolbarModule,
   MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatIconRegistry,
 } from '@angular/material';
 import {
   MatDialogModule,
   MatDialogActions,
   MatDialog,
   MatDialogContent,
-  MatDialogRef
+  MatDialogRef,
 } from '@angular/material/dialog';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatDialogModule, MatCardModule, ],
-  exports: [MatButtonModule, MatToolbarModule, MatDialogModule, MatDialogActions, MatDialogContent, MatCardModule, ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatDialogActions,
+    MatDialogContent,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
   declarations: []
 })
-export class MaterialModuleModule {}
+export class MaterialModuleModule {
+  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
+    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg'));
+  }
+}
