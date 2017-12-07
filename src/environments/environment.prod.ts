09256ec78e2ba91d1c680cae548ce3b0a65b3b35
Joshua Lewis <joshilewis@gmail.com>
2017-12-07 10:36:38 +0200

    layout: swaps 'Learn more' and 'Let me in' buttons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyABhE4qBhf9Djz-wJPQ4E6lfqt77iN-nmg',
-    authDomain: 'lendtome-93c5c.firebaseapp.com',
-    databaseURL: 'https://lendtome-93c5c.firebaseio.com',
-    projectId: 'lendtome-93c5c',
-    storageBucket: 'lendtome-93c5c.appspot.com',
-    messagingSenderId: '937784623563'
+    apiKey: "AIzaSyABhE4qBhf9Djz-wJPQ4E6lfqt77iN-nmg",
+    authDomain: "lendtome-93c5c.firebaseapp.com",
+    databaseURL: "https://lendtome-93c5c.firebaseio.com",
+    projectId: "lendtome-93c5c",
+    storageBucket: "lendtome-93c5c.appspot.com",
+    messagingSenderId: "937784623563"
   },
-  apiUrl: 'https://lend-to.me/api',
+  apiUrl: "https://lend-to.me/api"
 };
